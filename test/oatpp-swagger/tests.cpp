bc12d4619544548b95d273e9be04431d1d596553
lganzzzo <lganzzzo@gmail.com>
2019-01-21 13:06:21 +0200

    basic restructuring

---
+++ test/oatpp-swagger/tests.cpp
@@ -0,0 +1,52 @@
+
+#include "oatpp/core/concurrency/SpinLock.hpp"
+#include "oatpp/core/base/Environment.hpp"
+
+#include <iostream>
+
+namespace {
+
+class Logger : public oatpp::base::Logger {
+private:
+  oatpp::concurrency::SpinLock::Atom m_atom;
+public:
+
+  Logger()
+  : m_atom(false)
+  {}
+
+  void log(v_int32 priority, const std::string& tag, const std::string& message) override {
+    oatpp::concurrency::SpinLock lock(m_atom);
+    std::cout << tag << ":" << message << "\n";
+  }
+
+};
+
+void runTests() {
+  OATPP_LOGD("test", "insert oatpp-swagger tests here");
+}
+
+}
+
+int main() {
+
+  oatpp::base::Environment::init();
+  oatpp::base::Environment::setLogger(new Logger());
+
+  runTests();
+
+  oatpp::base::Environment::setLogger(nullptr);
+  oatpp::base::Environment::destroy();
+
+  /* Print how much objects were created during app running, and what have left-probably leaked */
+  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+  std::cout << "\nEnvironment:\n";
+  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
+  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
+
+  OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
+
+  oatpp::base::Environment::destroy();
+
+  return 0;
+}
